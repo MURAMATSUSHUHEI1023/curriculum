071b514a8d15fbef8406743bc807b85f5c3779f6
MURAMATSUSHUHEI1023 <117163346+MURAMATSUSHUHEI1023@users.noreply.github.com>
2022-12-29 17:10:09 +0900

    課題提出

---
+++ JavaStudy/Task1_1.java
@@ -0,0 +1,19 @@
+package JavaStudy;
+
+public class Task1_1 {
+    public static void main(String[] args) {
+        
+        int hogeInt =50;
+    String hogeString ="テスト";
+
+    double hogeDouble = 0.2;
+//少数を扱えるdouble型で0.2を初期化
+    hogeInt =100;
+    hogeString ="合格";
+
+    System.out.println(hogeInt);
+    System.out.println(hogeDouble);
+    System.out.println(hogeString);
+    //変数 hogeString の値を表示
+    }
+}
